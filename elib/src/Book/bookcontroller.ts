86d6278090e28688440b70313d4b6482c2b87f5a
manjeet98 <manjeet9871124@gmail.com>
2025-03-02 07:27:13 +0530

    set a multer

--- elib/src/Book/bookcontroller.ts
+++ elib/src/Book/bookcontroller.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express"
 
 const Createbook = async (req : Request, res : Response, next : NextFunction) => {
     res.json({message : 'welcome to elib api'})
+
+
 }
 
-export default Createbook;
+export {Createbook};
--- elib/src/Book/bookrouter.ts
+++ elib/src/Book/bookrouter.ts
@@ -2,15 +2,26 @@
 import express from 'express';
 
 const bookrouter = express.Router();
-import Createbook from './bookcontroller';
+import {Createbook} from './bookcontroller';
 import multer from 'multer';
+import path from 'path';
 
 
 const upload = multer({
-    
+    dest : path.resolve(__dirname, '../../public/data/uploads'),
+    limits : {fileSize : 3e7} // 30mb - 30*1024*1024
 })
 
-bookrouter.post('/', Createbook);
+
+bookrouter.post('/', upload.fields([
+    {
+        name : 'coverImage', maxCount : 1
+    },
+
+    {
+        name : 'file', maxCount : 1
+    }
+]) , Createbook);
 
 
 export default bookrouter;
