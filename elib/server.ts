e21b9c58e9f4e7dc4e99291781556c02bbde7511
manjeet98 <manjeet9871124@gmail.com>
2025-04-19 16:42:49 +0530

    create enc and confid file

--- elib/server.ts
+++ elib/server.ts
@@ -1,1 +1,12 @@
-console.log("iugfjkkjdf kjdjk");
+import {config} from "./src/config/config";
+import app from "./src/app";
+
+
+const startserver = () => {
+    const port =  config.port;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+startserver();
--- elib/src/app.ts
+++ elib/src/app.ts
@@ -1,2 +1,17 @@
 import express from "express";
-import { connectDB } from "./config/db";
+// import { connectDB } from "./config/db";
+
+
+const app = express();
+
+//Routes
+
+// HTTP methods
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+
+
+export default app; 
