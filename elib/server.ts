0c0d7c4a840111996b0c5eb04f311c136c0455b8
manjeet98 <manjeet9871124@gmail.com>
2025-04-23 22:59:19 +0530

    manjeet commit

--- elib/server.ts
+++ elib/server.ts
@@ -2,11 +2,11 @@ import {config} from "./src/config/config";
 import app from "./src/app";
 
 
-const startserver = () => {
+const Startserver = () => {
     const port =  config.port;
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
 }
 
-startserver();
+Startserver();
