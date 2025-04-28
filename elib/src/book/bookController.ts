f12b4d07ea3e4a4b94ea4486aa6550708f97dfe5
manjeet98 <manjeet9871124@gmail.com>
2025-04-28 11:05:01 +0530

    manjeet commit

--- elib/src/book/bookController.ts
+++ elib/src/book/bookController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const createBook = async (req : Request, res : Response, next : NextFunction) => {
   
+    console.log("files", req.files);
     res.json({message : "hello world"});
 
 }
---
+++ elib/src/config/cloudinary.ts
@@ -0,0 +1,15 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dnx27udjn', 
+        api_key: '195865957929279', 
+        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    });
+
+    export default cloudinary;
+    
+    
+  
