79077007f4ed187f607de50ee2d90fba1a11f5ae
manjeet98 <manjeet9871124@gmail.com>
2025-04-28 09:27:49 +0530

    manjeet commit

--- elib/src/book/bookroutes.ts
+++ elib/src/book/bookroutes.ts
@@ -1,11 +1,26 @@
 import express from 'express';
 import { createBook } from './bookController';
+import path from 'node:path';
+import multer from 'multer';
 
 const bookrouter = express.Router();
 
 
+const upload = multer({
+    dest : path.resolve(__dirname, '../../public/uploads'),
+    limits : {fileSize : 3e7}
+})
 
-bookrouter.post("/", createBook);
+
+bookrouter.post(
+    "/",
+
+    upload.fields([
+      {name : 'coverImage', maxCount : 1},
+      {name : 'file', maxCount : 1},
+    ]),
+    
+    createBook);
 
 
 
