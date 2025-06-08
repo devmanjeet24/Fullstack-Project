76b226d25610e3d8b373892b61320de431eb74f5
manjeet98 <manjeet9871124@gmail.com>
2025-06-08 16:34:43 +0530

    manjeet commit

--- elib-client-app/src/app/(home)/components/BookList.tsx
+++ elib-client-app/src/app/(home)/components/BookList.tsx
@@ -5,7 +5,7 @@ import Bookcards from './Bookcards';
 const BookList = ({books} : {books : Book[]}) => {
   return (
 
-    <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto'>
+    <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10'>
       {
         books.map((book, index) => {
           return(
