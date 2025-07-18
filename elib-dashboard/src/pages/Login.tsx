67248b56fd3a3d1b602d944b20e9e37b75ec7fd0
manjeet98 <manjeet9871124@gmail.com>
2025-07-18 22:54:49 +0530

    new comit

--- elib-dashboard/src/pages/Login.tsx
+++ elib-dashboard/src/pages/Login.tsx
@@ -1,9 +1,59 @@
+// import { cn } from "@/lib/utils"
+// import { Button } from "@/components/ui/button"
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
 
+// const Loginpage = () => {
+//   return (
+
+//   )
+// }
+
+// export default Loginpage;
+
+import { LoginForm } from "@/components/login-form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const Loginpage = () => {
   return (
-    <div>Loginpage</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+        <Popover>
+          <PopoverTrigger>Open</PopoverTrigger>
+          <PopoverContent>Place content for the popover here.</PopoverContent>
+        </Popover>
+
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
   )
 }
+
 
 export default Loginpage;
