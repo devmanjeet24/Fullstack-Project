d3029a07e5466cbf58a1cc51a40a6bfe7dacc28f
manjeet98 <manjeet9871124@gmail.com>
2025-09-18 06:19:30 +0530

    token save

--- elib-dashboard/src/Layout/AuthLayout.tsx
+++ elib-dashboard/src/Layout/AuthLayout.tsx
@@ -1,7 +1,15 @@
 
-import { Outlet } from 'react-router-dom';
+import useTokenstore from '@/store';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
+
+  const token = useTokenstore((state) => state.token);
+
+  if (token){
+    return <Navigate to={'/dashboard/home'} replace />
+  }
+
   return (
     <>
       <Outlet />
--- elib-dashboard/src/Layout/DashboardLayout.tsx
+++ elib-dashboard/src/Layout/DashboardLayout.tsx
@@ -14,9 +14,19 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { Input } from "@/components/ui/input"
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import useTokenstore from "@/store"
 
-  const DashboardLayout = () => {
+const DashboardLayout = () => {
+
+
+  const token = useTokenstore((state) => state.token);
+
+  if (token === ""){
+    return <Navigate to={'/auth/login'} replace />
+  }
+
+
   return (
     <>      <SidebarProvider>
       <AppSidebar />
@@ -46,14 +56,7 @@ import { Outlet } from "react-router-dom"
             <Input type="search" placeholder="Search" />
           </div>
         </header>
-        {/* <div className="flex flex-1 flex-col gap-4 p-4">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-              <div className="bg-muted/50 aspect-video rounded-xl" />
-              <div className="bg-muted/50 aspect-video rounded-xl" />
-              <div className="bg-muted/50 aspect-video rounded-xl" />
-            </div>
-            <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-          </div> */}
+
 
 
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
